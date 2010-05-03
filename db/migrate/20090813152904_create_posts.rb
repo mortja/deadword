872c442e42c6dc06e112feca5d25c0ef91ff84f2
unknown <mort@.(none)>
2010-05-03 17:41:43 -0500

    Uploaded to Heroku

---
+++ db/migrate/20090813152904_create_posts.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration
+  def self.up
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.datetime :timestamp
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :posts
+  end
+end
